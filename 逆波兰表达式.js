cb56b2ff9a03d703a780f4ba204d9f8b99fae084
janeluck <janeluck158@gmail.com>
2017-07-31 22:35:01 +0800

    中缀表达式转逆波兰表达式

--- 逆波兰表达式.js
+++ 逆波兰表达式.js
@@ -13,29 +13,41 @@ function toPPN(origin) {
     let ppn = [], operator = [];
 
     // 遍历中缀表达式转换成的数组
-    for (let i = 0; i < origin.length; i++){
+    for (let i = 0; i < origin.length; i++) {
         const item = origin[i]
         // 判断该位置上的元素是否为数字
-        if (!isNaN(item)){
+        if (!isNaN(item)) {
             // 如果是数字， item推入ppn
             ppn.push(item)
         } else if (item === '(' || operator.length === 0) {
             // 如果是'('或者operator为空数组, item推入operator
             operator.push(item)
-        } else if (0){
-            
+        } else if (item === ')') {
+            // 如果是')', operator从末尾弹出运算符并推入ppn直到'('
+            do {
+                ppn.push(operator.pop())
+            } while (operator[operator.length - 1] !== '(')
+            // operator弹出'('
+            operator.pop()
+        } else if (needPop(item, operator[operator.length - 1])) {
+
+
+
+
+        } else {
+            operator.push(item)
         }
     }
-
-
 
 
     return ppn
 }
 
 
-function compareOperatorPriority(a, b) {
+function needPop(a, b) {
+    // b: operator的尾元素
+    if (b === '(') return false
+    if ((a === '*' || a === '/') && (b === '+' || b === '-')) return false
+    return true
 
-
-    
 }
